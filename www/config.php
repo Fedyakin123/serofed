89e6ff65ce413a4d4504a80bd9b0707c9a3d2284
Fedyakin123 <Skabaz@yandex.ru>
2015-11-04 23:05:47 +0300

    ver. 0.02 3.11.15

--- www/config.php
+++ www/config.php
@@ -8,7 +8,7 @@
 define('SITE_DIR','http://' . $_SERVER['HTTP_HOST'] . '/');
 define('BOSS_PASSWORD', 'c622aac9231171298530353ac1b97f29');
 define('BOSS_LOGIN', 'Sergey');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_USER', 'serofed');
+define('DB_PASSWORD', '21091954');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'serofed');
