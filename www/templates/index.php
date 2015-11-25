8897ba11fefdb34cc5590d8aad11fa0c57f51032
Fedyakin123 <Skabaz@yandex.ru>
2015-11-25 10:17:40 +0300

    ver. 0.07 25.11.15

--- www/templates/index.php
+++ www/templates/index.php
@@ -1,6 +1,6 @@
 <?php  define('SITE_DIR','http://' . $_SERVER['HTTP_HOST'] .'/'); ?>
 
-<link rel="stylesheet" type="text/css" href="<?php echo SITE_DIR; ?>styles/main_style.css">
+<link rel="stylesheet" type="text/css" href="<?php echo SITE_DIR; ?>serofed/www/styles/main_style.css">
 
 <?php
 echo '<img style="margin: 0 auto; " src="' . SITE_DIR . 'serofed/www/img/restricted.png" />';
