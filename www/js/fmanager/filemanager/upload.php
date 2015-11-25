ec2a28ef9614e30bf4088250952ba0cb63937e03
Fedyakin123 <Skabaz@yandex.ru>
2015-11-25 04:44:22 +0300

    ver. 0.06 25.11.15

--- www/js/fmanager/filemanager/upload.php
+++ www/js/fmanager/filemanager/upload.php
@@ -84,8 +84,9 @@ if ( ! empty($_FILES))
 		else $is_img=FALSE;
 
 		// upload
+		echo '  111  '.$tempFile.'  111  ';
 		move_uploaded_file($tempFile,$targetFile);
-		echo '000'.$tempFile.'000'.$targetFile.'0000';
+		echo '  000  '.$targetFile.'  0000  ';
 		chmod($targetFile, 0755);
 
 		if ($is_img)
