17ca995fcf5c7f24a5b0406a7ae11afbfab62678
Fedyakin123 <Skabaz@yandex.ru>
2015-11-25 10:14:14 +0300

    ver. 0.07 25.11.15

--- www/templates/index.php
+++ www/templates/index.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" type="text/css" href="<?php echo SITE_DIR; ?>styles/main_style.css">
+
 <?php
 define('SITE_DIR','http://' . $_SERVER['HTTP_HOST'] . '/');
 echo '<img style="margin: 0 auto; " src="' . SITE_DIR . 'serofed/www/img/restricted.png" />';
