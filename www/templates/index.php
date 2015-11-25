f5553a97e080ff37eca010745ad80325f9d72635
Fedyakin123 <Skabaz@yandex.ru>
2015-11-25 10:15:38 +0300

    ver. 0.07 25.11.15

--- www/templates/index.php
+++ www/templates/index.php
@@ -3,4 +3,5 @@
 <?php
 define('SITE_DIR','http://' . $_SERVER['HTTP_HOST'] . '/');
 echo '<img style="margin: 0 auto; " src="' . SITE_DIR . 'serofed/www/img/restricted.png" />';
-?>
+//Выровнять по центру эту ебань, придумать другие заглушки
+?> 
