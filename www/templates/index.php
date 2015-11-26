bdf78db9e1d502a9411e7c84304e5995c27ddece
Fedyakin123 <Skabaz@yandex.ru>
2015-11-26 21:54:02 +0300

    ver. 0.09 26.11.15

--- www/templates/index.php
+++ www/templates/index.php
@@ -1,8 +1,8 @@
 <?php  define('SITE_DIR','http://' . $_SERVER['HTTP_HOST'] .'/'); ?>
 
-<link rel="stylesheet" type="text/css" href="<?php echo SITE_DIR; ?>serofed/www/styles/main_style.css">
+<link rel="stylesheet" type="text/css" href="<?php echo SITE_DIR; ?>styles/main_style.css">
 
 <?php
-echo '<img class="center" src="' . SITE_DIR . 'serofed/www/img/restricted.png" />';
+echo '<img class="center" src="' . SITE_DIR . 'img/restricted.png" />';
 //Выровнять по центру эту ебань, придумать другие заглушки
 ?>
