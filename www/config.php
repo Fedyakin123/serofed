c53e2d2f3a6fb8c692d4d5d6dd225fde3daf0696
Fedyakin123 <Skabaz@yandex.ru>
2015-12-28 14:01:02 +0300

    ver. 0.11.02  27.12.15  Изменен пароль

--- www/config.php
+++ www/config.php
@@ -6,7 +6,7 @@
  * Time: 0:01
  */
 define('SITE_DIR','http://' . $_SERVER['HTTP_HOST'] . '/');
-define('BOSS_PASSWORD', 'd1c0f7d93f8702eecff05ee08e491ed1');
+define('BOSS_PASSWORD', 'b002a6a4406f92be56f8843d467508f2');
 define('BOSS_LOGIN', 'Sergey');
 define('DB_USER', 'serofed');
 define('DB_PASSWORD', '21091954');
